d48128edd74f840c80699f98b2fd4b05f928af82
fedora800 <samshinde@gmail.com>
2023-03-19 12:27:14 +0000

    added arg 1

--- get_server_stats.sh
+++ get_server_stats.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-# format : Hostname, DateTime, CPU%, Mem%, Disk%, Network connections, ActiveUsers
+# Input is the remote hostname (Note: ssh keys should have been setup already for password less ssh)
 
-R_HOSTNAME=$(hostname)
+R_HOSTNAME=$1
 R_DATE=$(date +"%F %T")
 R_CPU_PCT_USED=$(top -b -n1 | grep Cpu | awk '{print $2}')
 R_MEM_PCT_USED=$(free | grep Mem | awk '{print $3/$2 *100.0}')
 R_DISK_PCT_USED=$(df -PTh | grep " /$" | awk '{print $6}' | sed 's/%//g')   # note, only looking for the main / mount
 R_NET_CONNS=$(ss -tp | grep ESTAB | wc -l)  # does a basic TCP connections count
 R_ACTIVE_USERS=$(who | wc -l)
+STATS_FILE="/tmp/remote_host_stats.csv"
 
-echo "Hostname, DateTime, CPU%, Mem%, Disk%"
-echo "$R_HOSTNAME, $R_DATE, $R_CPU_PCT_USED, $R_MEM_PCT_USED, $R_DISK_PCT_USED, $R_NET_CONNS, $R_ACTIVE_USERS"
+echo "Hostname, DateTime, CPU%, Mem%, Disk%" >> $STATS_FILE
+echo "$R_HOSTNAME, $R_DATE, $R_CPU_PCT_USED, $R_MEM_PCT_USED, $R_DISK_PCT_USED, $R_NET_CONNS, $R_ACTIVE_USERS" >> $STATS_FILE
