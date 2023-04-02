75f157d011557a6892236831d2f039f4e66e5a72
fedora800 <124460971+fedora800@users.noreply.github.com>
2023-04-02 15:51:55 +0100

    Update get_server_stats.sh

--- get_server_stats.sh
+++ get_server_stats.sh
@@ -3,8 +3,8 @@
 
 R_HOSTNAME=$1
 R_DATE=$(date +"%F %T")
-R_CPU_PCT_USED=$(top -b -n1 | grep Cpu | awk '{print $2}')
-R_MEM_PCT_USED=$(free | grep Mem | awk '{print $3/$2 *100.0}')
+R_CPU_PCT_USED=$(top -b -n1 | grep "^%Cpu" | awk '{print $2}')
+R_MEM_PCT_USED=$(free | grep Mem | awk '{printf("%4.2f", $3/$2 *100.0)}')
 R_DISK_PCT_USED=$(df -PTh | grep " /$" | awk '{print $6}' | sed 's/%//g')   # note, only looking for the main / mount
 R_NET_CONNS=$(ss -tp | grep ESTAB | wc -l)  # does a basic TCP connections count
 R_ACTIVE_USERS=$(who | wc -l)
