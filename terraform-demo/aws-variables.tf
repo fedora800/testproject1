6ab9b134c1e24c08f04e4940432faf0d93cbfef5
fedora800 <samshinde@gmail.com>
2023-04-09 09:53:33 +0000

    new terraform demo work

---
+++ terraform-demo/aws-variables.tf
@@ -0,0 +1,10 @@
+variable "access_key" {
+  type = string
+  default = "AKIAZTTPKEIGGGY2A7PQ"
+}
+
+variable "secret_key" {
+  type = string
+  default = "i2ieXUY7TNrb8mwnPBkM+XhWIuSAgQxIOt6IpOVU"
+}
+
