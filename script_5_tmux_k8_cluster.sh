ac9059e24b37a942df033d74f7c0911ff919e649
fedora800 <fedora800@example.com>
2023-05-04 11:42:15 +0000

    tmux script changes

--- script_5_tmux_k8_cluster.sh
+++ script_5_tmux_k8_cluster.sh
@@ -13,6 +13,7 @@ USER_ID="cloud_user"
 #172.31.29.163   acg-k8-worker2
 #172.31.25.15    acg-k8-worker3
 
+LHOST=$HOSTNAME
 RHOST_1="acg-k8-controlp2"
 RHOST_2="acg-k8-worker1"
 RHOST_3="acg-k8-worker2"
@@ -20,19 +21,20 @@ RHOST_4="acg-k8-worker3"
 
 # create a detached session, give it a session name and open up a localhost bash shell as window 1
 # a detached session means it will be created, but it will return us to the bash prompt, and we can attach to it when required
-tmux new-session -d -P -s ${SESSION_NAME} "bash"
+tmux new-session -P -d -s ${SESSION_NAME} -n ${LHOST}_A "bash"
 
 # create multiple windows and attach to the same session name as above, also print a message about it 
-tmux new-window -P -t ${SESSION_NAME}:2 -n ${RHOST_1%%.*} "ssh ${USER_ID}@${RHOST_1}"
-tmux new-window -P -t ${SESSION_NAME}:3 -n ${RHOST_2%%.*} "ssh ${USER_ID}@${RHOST_2}"
-tmux new-window -P -t ${SESSION_NAME}:4 -n ${RHOST_3%%.*} "ssh ${USER_ID}@${RHOST_3}"
-tmux new-window -P -t ${SESSION_NAME}:5 -n ${RHOST_4%%.*} "ssh ${USER_ID}@${RHOST_4}"
-tmux new-window -P -t ${SESSION_NAME}:6 -n MULT_NODES "bash"   # this last window will be for the multiple panes
+tmux new-window -P -t ${SESSION_NAME}:2 -n ${LHOST}_B     "bash"
+tmux new-window -P -t ${SESSION_NAME}:3 -n ${RHOST_1%%.*} "ssh ${USER_ID}@${RHOST_1}"
+tmux new-window -P -t ${SESSION_NAME}:4 -n ${RHOST_2%%.*} "ssh ${USER_ID}@${RHOST_2}"
+tmux new-window -P -t ${SESSION_NAME}:5 -n ${RHOST_3%%.*} "ssh ${USER_ID}@${RHOST_3}"
+tmux new-window -P -t ${SESSION_NAME}:6 -n ${RHOST_4%%.*} "ssh ${USER_ID}@${RHOST_4}"
+tmux new-window -P -t ${SESSION_NAME}:7 -n MULT_NODES "bash"   # this last window will be for the multiple panes
 
 #--------------------------------------------------------------------------------
 
 
-# window 6 created above has pane 1 on creation
+# window 7 created above has pane 1 on creation
 tmux send-keys "echo this is window-6 pane-1" Enter
 tmux select-pane -T "==${HOSTNAME}==" \; select-layout tiled
 
