976fc9d0e52312edc0b2d8bb57824d5a7c819e72
fedora800 <fedora800@example.com>
2025-01-31 15:48:07 +0000

    renamed script for tmux to the OCI hosts

--- tmux_3_oci_hosts.sh
+++ tmux_3_oci_hosts.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+# tmux_3_oci_hosts.sh
   
-SESSION_NAME="OCI"
+SESSION_NAME="OCI-NODES"
 USER_ID="ubuntu"
-alias tmux="tmux -f ~/git-projects/dotfiles/.tmux.conf"
 
 LHOST=$HOSTNAME
-RHOST_1="oci-k8-control1"
-RHOST_2="oci-k8-worker1"
+RHOST_1="oci-cpane1"
+RHOST_2="oci-worker1"
+RHOST_3="oci-worker2"
 
 # create a detached session, give it a session name and open up a localhost bash shell as window 1
 # a detached session means it will be created, but it will return us to the bash prompt, and we can attach to it when required
@@ -18,11 +19,14 @@ tmux new-window -P -t ${SESSION_NAME}:3 -n ${RHOST_1%%.*} "ssh ${USER_ID}@${RHOS
 tmux select-pane -T "${RHOST_1}"  # this is required as otherwise the pane title and status bar right hostname will not show RHOST but LHOST
 tmux new-window -P -t ${SESSION_NAME}:4 -n ${RHOST_2%%.*} "ssh ${USER_ID}@${RHOST_2}"
 tmux select-pane -T "${RHOST_2}"
-tmux new-window -P -t ${SESSION_NAME}:5 -n MULT_NODES "bash"   # this last window will be for the multiple panes
+tmux new-window -P -t ${SESSION_NAME}:5 -n ${RHOST_3%%.*} "ssh ${USER_ID}@${RHOST_3}"
+tmux select-pane -T "${RHOST_3}"
+tmux new-window -P -t ${SESSION_NAME}:6 -n MULT_NODES "bash"   # this last window will be for the multiple panes
 
 #--------------------------------------------------------------------------------
 
-# window 4 created above has pane 1 on creation
+
+# window 5 created above has pane 1 on creation
 tmux send-keys "echo this is window-6 pane-1" Enter
 tmux select-pane -T "${LHOST}" \; select-layout tiled
 
@@ -33,6 +37,10 @@ tmux select-pane -T "${RHOST_1}" \; select-layout tiled
 # create pane 3
 tmux split-window "ssh ${USER_ID}@$RHOST_2"
 tmux select-pane -T "${RHOST_2}" \; select-layout tiled
+
+# create pane 4
+tmux split-window "ssh ${USER_ID}@$RHOST_3"
+tmux select-pane -T "${RHOST_4}" \; select-layout tiled
 
 # now attach to this session to use it
 tmux attach-session -t ${SESSION_NAME}
